866e2d7ff7c6144c761ea0f3fddcbc48d44f238f
Pasit <enqueteplu@gmail.com>
2017-05-27 13:52:53 +0700

    test shell script update

--- create_user_test.sh
+++ create_user_test.sh
@@ -2,12 +2,20 @@
 #update os
 # sudo apt-get update
 #add user
-useradd -d /var/www/html/u/13580183 -s /bin/false 13580183
+useradd -d /var/www/html/u/13580184 -s /bin/false 13580184
 #create folder
-mkdir -p /var/www/html/u/13580183
+mkdir -p /var/www/html/u/13580184
 #Permission User to access folder
-sudo chown -R test_ftp /var/www/html/u/13580183/
+sudo chown -R test_ftp /var/www/html/u/13580184/
 #Create Password
-sudo passwd u13580183
-Z#K}Va9x\\
-Z#K}Va9x\\
+sudo passwd u13580184
+# Z#K}Va9x\\
+# Z#K}Va9x\\
+
+#!/usr/bin/expect
+spawn passwd foo
+expect "password:"
+send "fu9C:6Tg8k"
+expect "Retype new password:"
+send "fu9C:6Tg8k"
+interact
