4744ca5435e79e815f40ac8a332a3829f4d6bfbf
Pasit <enqueteplu@gmail.com>
2017-05-27 13:49:56 +0700

    test shell script update

--- create_user_test.sh
+++ create_user_test.sh
@@ -1,13 +1,13 @@
 # sshpass -p 'sss2017'  ssh root@128.199.168.53
 #update os
-sudo apt-get update
+# sudo apt-get update
 #add user
-useradd -d /var/www/html/u/13580182 -s /bin/false 13580182
+useradd -d /var/www/html/u/13580183 -s /bin/false 13580183
 #create folder
-mkdir -p /var/www/html/u/13580182
+mkdir -p /var/www/html/u/13580183
 #Permission User to access folder
-sudo chown -R test_ftp /var/www/html/u/13580182/
+sudo chown -R test_ftp /var/www/html/u/13580183/
 #Create Password
-sudo passwd u13580182
-echo "Kz1<Sp0A(T"
-echo "Kz1<Sp0A(T"
+sudo passwd u13580183
+Z#K}Va9x\\
+Z#K}Va9x\\
